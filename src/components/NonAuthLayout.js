262ab4a0330d89c524e4cb3c0203d6d61da3fa4b
Manmohan786 <Manmohan80gupta@gmail.com>
2020-09-26 13:56:31 +0530

    Final Changes

--- src/components/NonAuthLayout.js
+++ src/components/NonAuthLayout.js
@@ -4,19 +4,19 @@ import { withRouter } from "react-router-dom";
 class NonAuthLayout extends Component {
     constructor(props) {
         super(props);
-        this.state={};
+        this.state = {};
         this.capitalizeFirstLetter.bind(this);
     }
-    
+
     capitalizeFirstLetter = string => {
         return string.charAt(1).toUpperCase() + string.slice(2);
-      };
+    };
 
-    componentDidMount(){
+    componentDidMount() {
         let currentage = this.capitalizeFirstLetter(this.props.location.pathname);
 
         document.title =
-          currentage + " | Skote - Responsive Bootstrap 4 Admin Dashboard";
+            currentage + " | Gazerr";
     }
     render() {
         return <React.Fragment>
